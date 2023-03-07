d5772fd37d2401badbe89966c791cda5b3382e0a
venkatram-dev <45727389+venkatram-dev@users.noreply.github.com>
2023-03-06 17:11:56 -0800

    Add files via upload

--- project/pid_controller/pid_controller.cpp
+++ project/pid_controller/pid_controller.cpp
@@ -27,12 +27,14 @@ void PID::Init(double Kpi, double Kii, double Kdi, double output_lim_maxi, doubl
   tau_d = Kdi;
   tau_i = Kii;
   
+  //momentary variable to store temporary cross track error
+  cte_as_of_now = 0;
+  
   //initialize boundary condition variables
   output_limit_minimum = output_lim_mini;
   output_limit_maximum = output_lim_maxi;
   
-  //momentary variable to store temporary cross track error
-  double cte_as_of_now = 0;
+
 }
 
 
@@ -49,8 +51,8 @@ void PID::UpdateError(double cte) {
   
   double previous_cterror;
   previous_cterror = cte_as_of_now;
-  diff_cte = cte - previous_cterror;
-  int_cte += cte;
+  diff_cte = (cte - previous_cterror)/delta_time;
+  int_cte += (cte * delta_time);
   
   cte_as_of_now = cte;
   
@@ -69,6 +71,34 @@ double PID::TotalError() {
     //steer = -tau_p * cte - tau_d * diff_cte - tau_i * int_cte
     
     double tmp_control = -tau_p * cte - tau_d * diff_cte - tau_i * int_cte;
+    
+    cout << "delta_time" <<delta_time<<endl;
+    cout << "cte" << cte <<endl;
+    cout << "tau_p" << tau_p <<endl;
+    cout << "tau_d" << tau_d <<endl;
+    cout << "tau_i" << tau_i <<endl;
+    cout << "diff_cte" << diff_cte <<endl;
+    cout << "int_cte" << int_cte <<endl;
+    cout << "tmp_control" << tmp_control <<endl;
+    cout << "output_limit_minimum" << output_limit_minimum << endl;
+    cout << "output_limit_maximum" << output_limit_maximum << endl;
+    
+    // control needs to be with in boundary conditions
+    // ex output_lim_mini = 10, output_lim_maxi = 20
+    // if tmp_control = 30, we need output_lim_maxi 20 
+    // i tmp_control = 5, we need output_lim_mini 10
+    // if tmp_control = 15, we need 15
+    
+    // find if control is with in minimum boundary
+    if (tmp_control < output_limit_minimum){
+        tmp_control = output_limit_minimum;
+    }
+    // find if control is with in maximum boundary
+    if (tmp_control > output_limit_maximum){
+        tmp_control = output_limit_maximum;
+    }
+    
+    cout << "control after if" << tmp_control << endl;
       
     control = tmp_control;
     
@@ -80,4 +110,5 @@ double PID::UpdateDeltaTime(double new_delta_time) {
    * TODO: Update the delta time with new value
    */
   delta_time = new_delta_time;
+  return delta_time;
 }
